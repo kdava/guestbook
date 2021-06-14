93097c8234727a67c15ff60a30595fde8d1d9bd6
Keerthi <kdava.devops1@gmail.com>
2021-06-14 18:35:22 +0000

    fixed config for app

--- guestbook/istio.sh
+++ guestbook/istio.sh
@@ -1,3 +1,7 @@
+#Connecting to the created cluster
+echo "Connecting to cluster"
+gcloud container clusters get-credentials guestbook-cluster --region northamerica-northeast1 --project idyllic-pottery-316421
+
 # Istio Installlation steps
 echo "Installing istio"
 curl -L https://istio.io/downloadIstio | sh -
@@ -6,3 +10,4 @@ export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
 cd ..
 kubectl label namespace default istio-injection=enabled
+kubectl apply -f istio-manifests.yaml
